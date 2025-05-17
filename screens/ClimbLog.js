cdbc1c3240beeaf5b404f93c84da250ae1d42cde
TimmyRich <gotim02@gmail.com>
2025-05-17 21:49:03 +1000

    Added navigation to home screen

--- screens/ClimbLog.js
+++ screens/ClimbLog.js
@@ -1,27 +1,58 @@
 import { View, Text, StyleSheet } from 'react-native';
 import { Button } from 'react-native';
+import { SafeAreaView } from 'react-native';
 
-export default function ClimbLog() {
+export default function ClimbLog({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Welcome to the Climb Log!</Text>
-      <Button
-      title="Go Home"
-      onPress={console.log("Go Home")}
-      />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.titleFont}>Climbing Log</Text>
+      </View>
+      {/* Home Button */}
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonWrapper}>
+          <Button
+          title="Home"
+          onPress={() => navigation.goBack()}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lavender',
+    backgroundColor: 'bisque',
   },
-  text: {
-    fontSize: 24,
+  header: {
+    width: '100%',
+    paddingBottom: 20,
+    paddingTop: 40,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    shadowRadius: 1,
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+  },
+  titleFont: {
+    fontSize: 34,
     fontWeight: 'bold',
+    fontFamily: 'arial',
+    textTransform: 'uppercase',
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    paddingTop: 20,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  buttonWrapper: {
+    marginBottom: 10,
+    width: "50%"
   },
 });
