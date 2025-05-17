cbc6a448244efe88db6db58e81e51cee33881cf5
TimmyRich <gotim02@gmail.com>
2025-05-17 21:18:49 +1000

    Renamed index.js to app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import App from './app/Home';
+import App from './screens/Home';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
