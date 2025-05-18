a6dae35a175ef24d67b7d8ae980ba6c05671ef81
TimmyRich <gotim02@gmail.com>
2025-05-18 12:18:33 +1000

    Climb class representing custom Climb data type.
    Climb class validates constructor args, if no date is provided, the current date is used. Currently doesn't parse a date.
    
    TODO: add date parsing

---
+++ data/climbs.js
@@ -0,0 +1,71 @@
+// Urban Climb locations
+export const URBAN_CLIMB_GYMS = [
+  "Milton",
+  "West End",
+  "Newstead"
+]
+
+// Locations within a gym
+export const GYM_ZONES = new Map([
+  ["Milton", ["The Cave"]],
+  ["West End", ["Stronarch"]],
+  ["Newstead", ["Big Bulge"]]
+])
+
+// Grade/Difficulty of a climb
+export const GRADES = [ // Wish this could be an enum :(
+  "Yellow",
+  "Blue",
+  "Purple",
+  "Green",
+  "Orange",
+  "Red",
+  "Black",
+  "White"
+]
+
+/**
+ * Represents a climbing route at an Urban Climb gym.
+ * 
+ * @property {string} gym - The Urban Climb location.
+ * @property {string} zone - The zone within the gym where the climb is located.
+ * @property {string} grade - The difficulty/grade of the climb
+ * @property {Date} date - The date the climb was registered.
+ * 
+ * This class defines key attributes of a climb, such as location, zone,
+ * difficulty, and date. Climbs are regularly reset, and each gym has 
+ * multiple zones with climbs of varied difficulty.
+ */
+export class Climb {
+  /**
+   * Create a new Climb.
+   * @param {string} gym - Urban Climb location
+   * @param {string} zone - Zone within the gym
+   * @param {string} grade - Climb difficulty
+   * @param {string} date - Date the climb was registered
+   */
+  constructor(gym, zone, grade, date) {
+
+    // Validate constructor args
+    if (!URBAN_CLIMB_GYMS.includes(gym)) {
+      throw new Error(`Invalid gym: ${gym}`);
+    }
+    if (!GYM_ZONES.get(gym).includes(zone)) {
+      throw new Error(`Invalid zone: ${zone}`);
+    }
+    if (!GRADES.includes(grade)) {
+      throw new Error(`Invalid grade: ${grade}`);
+    }
+    //TODO add date validator
+
+    // if no date was entered use current date
+    if (date == null) {
+      date = new Date();
+    }
+
+    this.gym = gym;
+    this.zone = zone;
+    this.grade = grade;
+    this.date = date;
+  }
+}
